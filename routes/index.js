bb19b8e1a0b9647837fe34950111ba0e520e83f3
Raksit <raksit.m@ku.th>
2019-08-16 13:45:49 +0700

    sent a request to create an event on stop-jeep stream using eventstore

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/users/:id/balances', async function(req, res, next) {
+  var headers = {
+    'ES-EventType': 'balanceUpdated'
+  };
+  var payload = {
+    account_id: req.params.id,
+    amount: 5
+  };
+
+  try {
+    await axios.post('http://localhost:2113/streams/stop-jeep', payload, { headers });
+    res.send('Successful');
+  } catch (error) {
+    res.send('Failed', error);
+  }
 });
 
 module.exports = router;
