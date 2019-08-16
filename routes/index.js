67cbbbc4de1bb7f3fb7a4f414af44fdaf7e23cab
Raksit <raksit.m@ku.th>
2019-08-16 16:54:09 +0700

    handled request body when send NaN to amount

--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,31 @@ var router = express.Router();
 var axios = require('axios');
 
 router.post('/users/:id/balances', async function(req, res, next) {
-  var headers = {
-    'ES-EventType': 'balanceUpdated'
-  };
-  var payload = {
-    account_id: req.params.id,
-    amount: req.body.amount
-  };
-
-  var hostname = process.env.EVENTSTORE_HOST;
-  var port = process.env.EVENTSTORE_PORT;
-
   try {
+    var amount = req.body.amount;
+    if (!isNumber(amount)) {
+      throw new Error('Accept only number')
+    }
+    
+    var headers = {
+      'ES-EventType': 'balanceUpdated'
+    };
+    var payload = {
+      account_id: req.params.id,
+      amount: parseInt(amount, 10)
+    };
+  
+    var hostname = process.env.EVENTSTORE_HOST;
+    var port = process.env.EVENTSTORE_PORT;
     await axios.post(`http://${hostname}:${port}/streams/stop-jeep`, payload, { headers });
-    res.send('Successful');
+    res.send(201, 'Successful');
   } catch (error) {
-    res.send('Failed', error);
+    res.send(400, error.message);
   }
 });
+
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
 
 module.exports = router;
